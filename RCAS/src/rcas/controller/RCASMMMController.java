466098ccea8501c184ce63b45e79819b14086cb3
Chris <christopher.oconnor@santismail.ch>
2019-04-15 12:49:43 +0200

    Added Moveable Labels to BartCharts

--- RCAS/src/rcas/controller/RCASMMMController.java
+++ RCAS/src/rcas/controller/RCASMMMController.java
@@ -5,16 +5,19 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import rcas.model.RaceCar;
 import rcas.util.CorneringAnalyserUtil;
@@ -36,11 +39,16 @@ public class RCASMMMController {
 
 	private CorneringAnalyserUtil util;
 	private ArrayList<RaceCar> raceCars;
+	private ArrayList<Text>   controlChartValueLabels;
+	private ArrayList<Text> stabilityChartValueLabels;
 
 	public RCASMMMController() {
 
 		this.util     = new CorneringAnalyserUtil();
 		this.raceCars = new ArrayList<>();
+
+		this.controlChartValueLabels   = new ArrayList<>();
+		this.stabilityChartValueLabels = new ArrayList<>();
 
 	}
 
@@ -115,7 +123,6 @@ public class RCASMMMController {
 
 			changeChartSize();
 
-
 		}
 
 	}
@@ -135,16 +142,21 @@ public class RCASMMMController {
 	@FXML
 	private void upDateChart(){
 
-		controlChart  .getData().clear();
-		stabilityChart.getData().clear();
-
 		double [] tfData = getTFData();
+		int i = 0;
 
-		for(RaceCar raceCar: raceCars) {
+		for (RaceCar raceCar :raceCars) {
 
-			addChartData(util.getMMMControlValue  (raceCar, tfData[0], tfData[1], tfData[2]), raceCar.getColor(), controlChart  );
-			addChartData(util.getMMMStabilityValue(raceCar, tfData[3], tfData[4], tfData[5]), raceCar.getColor(), stabilityChart);
+			final double controlValue   = (Math.round(util.getMMMControlValue  (raceCar, tfData[0], tfData[1], tfData[2]) * 100)) / 100;
+			final double stabilityValue = (Math.round(util.getMMMStabilityValue(raceCar, tfData[3], tfData[4], tfData[5]) * 100)) / 100;
 
+			controlChart  .getData().get(i).getData().get(0).setYValue(  controlValue);
+			stabilityChart.getData().get(i).getData().get(0).setYValue(stabilityValue);
+
+			controlChartValueLabels  .get(i).setText(controlValue   + "");
+			stabilityChartValueLabels.get(i).setText(stabilityValue + "");
+
+			i++;
 		}
 
 	}
@@ -153,11 +165,16 @@ public class RCASMMMController {
 
 		switch(raceCars.size()) {
 
-			case  2: setChartSize(120); break;
+			case  2: setChartSize(120);
+					 balanceChart  .setBarGap(2);
+					 gripChart     .setBarGap(2);
+					 controlChart  .setBarGap(2);
+					 stabilityChart.setBarGap(2);
+					 break;
 			case  3: setChartSize(100); break;
-			case  4: setChartSize( 80); break;
-			case  5: setChartSize( 60); break;
-			case  6: setChartSize( 40); break;
+			case  4: setChartSize( 60); break;
+			case  5: setChartSize( 30); break;
+			case  6: setChartSize( 10); break;
 
 		}
 
@@ -184,15 +201,16 @@ public class RCASMMMController {
 
 	private void addChartData(double value, String color, BarChart<String, Number> chart) {
 
-		value  = (Math.round(value * 100)) / 100;
+		final double VALUE  = (Math.round(value * 100)) / 100;
 	
 		XYChart.Series<String, Number> series = new XYChart.Series<>();
-		XYChart.Data  <String, Number> data   = new XYChart.Data<>("", value);/*createData(value, color);*/
+		XYChart.Data  <String, Number> data   = new XYChart.Data<>("", VALUE);
 
 		data.nodeProperty().addListener(new ChangeListener<Node>() {
 			@Override
 			public void changed(ObservableValue<? extends Node> observable, Node oldValue, final Node node) {
 				data.getNode().setStyle("-fx-bar-fill: " + color + ";");
+				displayLabelForData(data, chart);
 			}
 		});
 
@@ -201,19 +219,38 @@ public class RCASMMMController {
 
 	}
 
-	private XYChart.Data<String, Number> createData(double value, String color) {
-		XYChart.Data<String, Number> data =  new XYChart.Data<>("", value);
+	private void displayLabelForData(XYChart.Data<String, Number> data, BarChart<String, Number> chart) {
 
-		StackPane node = new StackPane();
-		Label label = new Label(String.valueOf(value));
-		label.setRotate(-90);
-		Group group = new Group(label);
-		StackPane.setAlignment(group, Pos.TOP_CENTER);
-		StackPane.setMargin(group, new Insets(0, 0, 5, 0));
-		node.getChildren().add(group);
-		data.setNode(node);
-		
-		return data;
+		Node node     = data.getNode();
+		Text dataText = new Text(data.getYValue() + "");
+
+		node.parentProperty().addListener(new ChangeListener<Parent>() {
+			@Override
+			public void changed(ObservableValue<? extends Parent> ov, Parent oldParent, Parent parent) {
+				Group parentGroup = (Group) parent;
+				if (parentGroup != null) {
+
+					ObservableList<Node> children = parentGroup.getChildren();
+					if (children != null)
+						children.add(dataText);
+				}
+			}
+		});
+
+		node.boundsInParentProperty().addListener(new ChangeListener<Bounds>() {
+			@Override
+			public void changed(ObservableValue<? extends Bounds> ov, Bounds oldBounds, Bounds bounds) {
+
+				dataText.setLayoutX(Math.round(bounds.getMinX() + bounds.getWidth() / 2 - dataText.prefWidth(-1) / 2));
+
+				if(Double.valueOf(dataText.getText()) > 0) dataText.setLayoutY(Math.round((bounds.getMinY()) - dataText.prefHeight(-1) * 0.1) + 20);
+				else dataText.setLayoutY(Math.round((bounds.getMaxY()) + dataText.prefHeight(-1)) - 25);
+
+			}
+		});
+
+		if(chart == controlChart)     controlChartValueLabels.add(dataText);
+		if(chart == stabilityChart) stabilityChartValueLabels.add(dataText);
 
 	}
 
